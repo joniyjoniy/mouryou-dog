73c21c65e70ce06738a9b59ed0e559b088c15811
joniyjoniy <joniyjoniylo@gmail.com>
2016-12-05 16:49:06 +0900

    add GetDiskIO

--- plugin.go
+++ plugin.go
@@ -9,53 +9,33 @@ import (
 	"os"
 	"os/signal"
 	"net/url"
+	"strings"
+	"fmt"
 	
 	"github.com/gorilla/websocket"
 	"github.com/shirou/gopsutil/mem"
 	// "github.com/shirou/gopsutil/cpu"
-	// "github.com/shirou/gopsutil/disk"
+	"github.com/shirou/gopsutil/disk"
 	"github.com/shirou/gopsutil/host"
 )
 
 type ServerStat struct {
 	// Host
-	HostName             string `json:"hostname"`
-	HostID               string `json:"hostid"`
-	VirtualizationSystem string `json:"virtualizationSystem"`
+	HostName             string  `json:"hostname"`
+	HostID               string  `json:"hostid"`
+	VirtualizationSystem string  `json:"virtualizationSystem"`
   // mem.VirtualMemoryStat
-	Total        uint64 `json:"total"`
-	Available    uint64 `json:"available"`
-	Used         uint64 `json:"used"`
-	UsedPercent  float64 `json:"usedPercent"`
-	Free         uint64 `json:"free"`
-	Active       uint64 `json:"active"`
-	Inactive     uint64 `json:"inactive"`
-	Wired        uint64 `json:"wired"`
-	Buffers      uint64 `json:"buffers"`
-	Cached       uint64 `json:"cached"`
-	Writeback    uint64 `json:"writeback"`
-	Dirty        uint64 `json:"dirty"`
-	WritebackTmp uint64 `json:"writebacktmp"`
-	// Cpu
-	// DiskIO
-	ReadCount        uint64 `json:"readCount"`
-	MergedReadCount  uint64 `json:"mergedReadCount"`
-	WriteCount       uint64 `json:"writeCount"`
-	MergedWriteCount uint64 `json:"mergedWriteCount"`
-	ReadBytes        uint64 `json:"readBytes"`
-	WriteBytes       uint64 `json:"writeBytes"`
-	ReadTime         uint64 `json:"readTime"`
-	WriteTime        uint64 `json:"writeTime"`
-	IopsInProgress   uint64 `json:"iopsInProgress"`
-	IoTime           uint64 `json:"ioTime"`
-	WeightedIO       uint64 `json:"weightedIO"`
-	Name             string `json:"name"`
-	SerialNumber     string `json:"serialNumber"`
-	
-	// Memory *mem.VirtualMemoryStat  `json:"-"`
+	Total                uint64  `json:"total"`
+	Available            uint64  `json:"available"`
+	UsedPercent          float64 `json:"usedPercent"`
+	// DiskIO map[string]disk.IOCountersStat
+	DiskIO string `json:"diskIO"`
+	// IoTime           uint64 `json:"ioTime"`
+	// WeightedIO       uint64 `json:"weightedIO"`
+
+	// Cpu	
 	// Cpu    []cpu.TimesStat         `json:"-"`
-	// DiskIO map[string]disk.IOCountersStat    `json:"-"`
-	// Host   host.InfoStat  `json:"-"`
+
 }
 
 var addr = flag.String("addr", "localhost:8080", "monitoring address")
@@ -83,12 +63,11 @@ func main() {
 		defer c.Close()
 		defer close(done)
 		for {
-			_, message, err := c.ReadMessage()
+			_, _, err := c.ReadMessage()
 			if err != nil {
 				log.Println("read:", err)
 				return
 			}
-			log.Printf("recv: %s", message)
 		}
 	}()
 
@@ -128,24 +107,42 @@ func GetServerStat() (ServerStat) {
 
 	d = d.GetHostStat()
   d = d.GetMemoryStat()
-	// d.GetDiskIOStat()
-	
+	d = d.GetDiskIOStat()	
 	return d
 }
 
 func (s ServerStat) GetHostStat() (ServerStat) {
 	h, _ := host.Info()
-	s.HostName = h.Hostname
+	s.HostName             = h.Hostname
+	s.HostID               = h.HostID
+	s.VirtualizationSystem = h.VirtualizationSystem
 	return s
 }
 
-func (s ServerStat) GetMemoryStat() (ServerStat){
+func (s ServerStat) GetMemoryStat() (ServerStat) {
 	m, _ := mem.VirtualMemory()
 	s.Total = m.Total
+	s.Available = m.Available
+	s.UsedPercent = m.UsedPercent
 	return s
 }
 
-// func (s ServerStat) GetDiskIOStat() {
-// 	i, _ := disk.IOCounters()
-// 	s.ReadCount = i.ReadCount
-// }
+func (s ServerStat) GetDiskIOStat() (ServerStat) {
+	i, _ := disk.IOCounters()
+	s.DiskIO = ConvertMapToString(i)
+	return s
+}
+
+func ConvertMapToString(m map[string]disk.IOCountersStat) (string) {
+	var str string
+
+	str = "{"
+	for k, v := range m {
+		str  = str + string(k) + ":{"
+		str = str + "ioTime:" + fmt.Sprint(v.IoTime) + ","
+		str = str + "weightedIO:" + fmt.Sprint(v.WeightedIO) + "},"
+	}
+  str = strings.TrimRight(str, ",")
+	str = str + "}"
+	return str
+}
